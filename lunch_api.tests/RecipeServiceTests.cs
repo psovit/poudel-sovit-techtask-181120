3dac7e26aac888f97a61ed40aa5920677736dd17
Sovit P <sovitp@Sovits-MacBook-Pro.local>
2020-11-18 15:13:05 +0545

    RecipeServiceTests: test case to check order of recipes based on Best By date

--- lunch_api.tests/RecipeServiceTests.cs
+++ lunch_api.tests/RecipeServiceTests.cs
@@ -188,5 +188,81 @@ namespace lunch_api.Tests
 
             Assert.Equal(result.Count, 1);
         }
+
+        [Fact]
+        public void BestBeforeOrderingTest()
+        {
+            var ingredient1 = new IngredientView()
+            {
+                Title = "Ham",
+                BestBefore = "2021-03-25",
+                UseBy = "2021-01-01"
+            };
+
+            var ingredient2 = new IngredientView()
+            {
+                Title = "Cheese",
+                BestBefore = "2020-01-01",
+                UseBy = "2021-01-01"
+            };
+
+            var ingredient3 = new IngredientView()
+            {
+                Title = "Beans",
+                BestBefore = "2021-04-01",
+                UseBy = "2021-01-01"
+            };
+
+            var lsIngredients = new List<IngredientView>()
+            {
+                ingredient1, ingredient2, ingredient3
+            };
+
+            var recipe1 =  new RecipeView()
+            {
+                Title = "Ham Toastie",
+                Ingredients = new List<string>()
+                {
+                    "Ham",
+                    "Bread",
+                    "Butter"
+                }
+            };
+
+            var recipe2 =  new RecipeView()
+            {
+                Title = "Cheese Toastie",
+                Ingredients = new List<string>()
+                {
+                    "Cheese",
+                    "Bread",
+                    "Butter"
+                }
+            };
+
+            var recipe3 =  new RecipeView()
+            {
+                Title = "Beans Toastie",
+                Ingredients = new List<string>()
+                {
+                    "Beans",
+                    "Bread",
+                    "Butter"
+                }
+            };
+
+            var lsRecipes = new List<RecipeView>()
+            {
+                recipe1, recipe2, recipe3
+            };
+
+            var ingredientRepo = new TestIngredientRepository(lsIngredients);
+            var recipeRepo = new TestRecipeRepository(lsRecipes);
+            var recipeService = new RecipeService(recipeRepo, ingredientRepo);
+            var result = recipeService.GetRecipes();
+
+            var lastRecipe = result[2];
+            Assert.Equal(recipe2, lastRecipe);
+        }
     }
 }
