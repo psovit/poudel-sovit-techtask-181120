3b992b278483f509282a645de8dfd3efd5dff32f
Sovit P <sovitp@Sovits-MacBook-Pro.local>
2020-11-18 14:37:14 +0545

    RecipeServiceTests: test case for no matching ingredient

--- lunch_api.tests/RecipeServiceTests.cs
+++ lunch_api.tests/RecipeServiceTests.cs
@@ -76,5 +76,60 @@ namespace lunch_api.Tests
 
             Assert.Equal(result.Count, 1);
         }
+
+        [Fact]
+        public void NoRecipesIfIngredientsDontMatchTest()
+        {
+            var ingredient1 = new IngredientView()
+            {
+                Title = "Ham",
+                BestBefore = "2021-03-25",
+                UseBy = "2021-01-01"
+            };
+
+            var ingredient2 = new IngredientView()
+            {
+                Title = "Cheese",
+                BestBefore = "2021-03-25",
+                UseBy = "2021-01-01"
+            };
+
+            var lsIngredients = new List<IngredientView>()
+            {
+                ingredient1, ingredient2
+            };
+
+            var recipe1 =  new RecipeView()
+            {
+                Title = "Bread Toastie",
+                Ingredients = new List<string>()
+                {
+                    "Bread",
+                    "Butter"
+                }
+            };
+
+            var recipe2 =  new RecipeView()
+            {
+                Title = "Rice Toastie",
+                Ingredients = new List<string>()
+                {
+                    "Rice",
+                    "Butter"
+                }
+            };
+
+            var lsRecipes = new List<RecipeView>()
+            {
+                recipe1, recipe2
+            };
+
+            var emptyIngredientRepo = new TestIngredientRepository(lsIngredients);
+            var emptyRecipeRepo = new TestRecipeRepository(lsRecipes);
+            var recipeService = new RecipeService(emptyRecipeRepo, emptyIngredientRepo);
+            var result = recipeService.GetRecipes();
+
+            Assert.Equal(result.Count, 0);
+        }
     }
 }
